264eb764fb69f04c7eb2ae5d36ad502f27b262e7
Unknown <marine9salamander@outlook.com>
2017-09-01 13:36:10 -0700

    WordBank.cs
    + Added method "SelectWord"
        - This returns a word that is selected at random from the wordbank.

--- WordBank.cs
+++ WordBank.cs
@@ -2,8 +2,22 @@ using System;
 
 public class WordBank
 {
-    public WordBank()
+    var bankFile = Path.Combine(Directory.GetCurrentDirectory(), "\\words.txt");
+    System.IO.StreamReader wordBank = new System.IO.StreamReader(bankFile);
+
+    private WordBank() // only this class needs to write to or use the word bank, if you need to get the words, use GetWordBank()
     {
         //oh no now i do the thing
+    }
+
+    public void GetWordBank()
+    {
+        //return wordBank;
+    }
+
+    public static string SelectWord()
+    {
+        string word = "zee";
+        return word;
     }
 }
