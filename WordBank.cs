0805ecfae3d38c13a802c47195d70baf4994ea95
Unknown <marine9salamander@outlook.com>
2017-09-02 22:38:58 -0700

    Attempted to make it use the console
    it did not work

--- WordBank.cs
+++ WordBank.cs
@@ -5,9 +5,23 @@ public class WordBank
     var bankFile = Path.Combine(Directory.GetCurrentDirectory(), "\\words.txt");
     System.IO.StreamReader wordBank = new System.IO.StreamReader(bankFile);
 
+[   ContentPropertyAttribute("Text")]
+    
     private WordBank() // only this class needs to write to or use the word bank, if you need to get the words, use GetWordBank()
     {
         //oh no now i do the thing
+        while ((line = bankFile.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+            counter++;
+        }
+        Console.ReadKey();
+    }
+
+    public static string GetWordBank()
+    {
+        WordBank();
+        return "Done!";
     }
 
     public static string SelectWord()
@@ -15,4 +29,12 @@ public class WordBank
         string word = "zee";
         return word;
     }
+
+    public static voice Main(string[] args)
+    {
+        WordBank returnBank = new WordBank.GetWordBank();
+        returnBank();
+    }
+
 }
+
