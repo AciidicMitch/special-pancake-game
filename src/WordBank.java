bb537a3e2e3eee1a4e7b55520d14d9d2827db5d0
Unknown <marine9salamander@outlook.com>
2017-09-05 12:19:07 -0700

    wordbanksss
    yoo

--- src/WordBank.java
+++ src/WordBank.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 
 public class WordBank {
-
+	File wordBank = new File("words.txt");
+	String word;
+	String[] bank;
+	
+	public String GetWord(){
+		if(word == null){
+			SelectWord();
+		}
+		
+		return word;
+	}
+	
+	private String SelectWord(){
+		try {
+			int lines = 0;
+			Scanner scan = new Scanner(wordBank);
+			while (scan.hasNextLine()) {
+                //String line = scan.toString();
+				System.out.println(scan.toString());
+                lines++;
+            }
+			
+		} catch (FileNotFoundException e){e.printStackTrace();}
+		
+		return word;
+	} 
 	
 	
 }
