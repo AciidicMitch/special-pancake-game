929152ff3c4cb8724dbb34b0a7661e35a3fd9d9c
Unknown <marine9salamander@outlook.com>
2017-09-07 11:59:26 -0700

    REEE

--- src/WordBank.java
+++ src/WordBank.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-
+import java.io.*;
 
 public class WordBank {
 	File wordBank = new File("words.txt");
@@ -17,13 +15,31 @@ public class WordBank {
 	}
 	
 	private String SelectWord(){
+		System.out.println("yooo");
 		try {
+			System.out.println("yooo");
 			int lines = 0;
 			Scanner scan = new Scanner(wordBank);
 			while (scan.hasNextLine()) {
+				//String line = scan.toString();
+				//System.out.println(scan.toString());
+				if(scan.nextLine() == null){
+					break;
+				}
+				else{lines++;}
+            }
+			System.out.println("yooo");
+			int selectedWord = (int)(Math.random() *lines) + 1;
+			System.out.println(selectedWord + " is the index location");
+			lines = 0;
+			while(true) {
                 //String line = scan.toString();
-				System.out.println(scan.toString());
-                lines++;
+				if(lines == selectedWord){
+					word = scan.nextLine();
+					break;
+				}
+				System.out.println(lines);
+				lines++;
             }
 			
 		} catch (FileNotFoundException e){e.printStackTrace();}
